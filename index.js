7ba80a2e10fb7438e559fbba71d0334f8f2d7292
Mohammad Solaiman <solaiman321@gmail.com>
2022-01-17 16:34:35 +0600

    added tracking via order id and courierserviceability

--- index.js
+++ index.js
@@ -1,20 +1,95 @@
-const Utils = require('./utils')
+const axios = require("axios").default;
+const Utils = require("./utils");
 
-
-//// Tracking
-function Tracking(options) {
-  console.log('tracking')
+///  Get Token
+async function GetToken({ email, password }) {
+  const token = await Utils.GetToken(email, password);
+  return token;
 }
 
+//// Get Tracking Data through Order iD
+async function Tracking_OrderId({ auth, params }) {
+  if (!auth.email && !auth.password && !auth.token) {
+    return console.log("pass the valid props");
+  }
 
+  if (!params) {
+    return console.log("order_id params is required");
+  }
 
+  var token = auth.token;
+  if (!token) {
+    token = await Utils.GetToken(auth.email, auth.password);
+  }
 
-///  PickupAddress 
-function PickupAddress(options) {
-  // console.log('pin code ')
-  Utils.GetToken()
+  const parameterGenerator = new URLSearchParams(params);
+  const parameter = parameterGenerator.toString();
 
+  const url = `https://apiv2.shiprocket.in/v1/external/courier/track?${parameter}`;
 
+  const myHeaders = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  };
+
+  const requestOptions = {
+    url,
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await axios(requestOptions);
+    const trackingDetails = await response.data;
+    return trackingDetails;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 }
 
-module.exports = {Tracking,PickupAddress};
+///  Check Courier Serviceability
+async function CourierServiceability({ auth, params }) {
+  if (!auth.email && !auth.password && !auth.token) {
+    return console.log("pass the valid props");
+  }
+
+  var token = auth.token;
+  if (!token) {
+    token = await Utils.GetToken(auth.email, auth.password);
+  }
+
+  const parameterGenerator = new URLSearchParams(params);
+  const parameter = parameterGenerator.toString();
+  const url = `https://apiv2.shiprocket.in/v1/external/courier/serviceability/?${parameter}`;
+
+  const myHeaders = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  };
+
+  const requestOptions = {
+    url,
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await axios(requestOptions);
+    if (response.status == "422") {
+      const errorMessage = "Invalid Pincode";
+      return errorMessage;
+    }
+
+    const json = response.data;
+    const estimatedDate = json.data.available_courier_companies[0].etd;
+    return estimatedDate;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+module.exports = { Tracking_OrderId, CourierServiceability, GetToken };
--- utils.js
+++ utils.js
@@ -1,8 +1,34 @@
-const GetToken = ()=>{
-        console.log('get token')
+const axios = require("axios").default;
 
+const GetToken = async (email, password) => {
+  const url = "https://apiv2.shiprocket.in/v1/external/auth/login";
+
+  const myHeaders = {
+    "Content-Type": "application/json",
+  };
+
+  const raw = JSON.stringify({
+    email: email,
+    password: password,
+  });
+
+  const requestOptions = {
+    method: "POST",
+    url,
+    headers: myHeaders,
+    data: raw,
+    redirect: "follow",
+  };
+
+  try {
+    const response = await axios(requestOptions);
+        const token = response.data.token 
+        return token
+
+  } catch (err) {
+    console.log(err);  
 }
 
+};
 
-
-module.exports = { GetToken}
+module.exports = { GetToken };
